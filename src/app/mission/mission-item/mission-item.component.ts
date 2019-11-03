1d44339c9fe6e54546c881d30cc6a12bbf7f69c2
szy200414 <szy200414@hotmail.com>
2019-11-03 17:16:27 +0100

    compare date

--- src/app/mission/mission-item/mission-item.component.ts
+++ src/app/mission/mission-item/mission-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Mission } from 'src/app/domain/mission.model';
+import { isBefore, } from 'date-fns';
+import { differenceInDays } from 'date-fns/esm';
 
 @Component({
   selector: 'app-mission-item',
@@ -7,16 +10,23 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MissionItemComponent implements OnInit {
 
-  @Input() mission;
+  @Input() mission: Mission;
   reminder: number;
 
   constructor() { }
 
   ngOnInit() {
     this.reminder = 0;
+    const dueDate = this.mission.DueDate;
+    if (isBefore(dueDate, Date.now())) {
+      this.reminder = 2;
+    } else if (differenceInDays(dueDate, Date.now()) < 2) {
+      this.reminder = 1;
+    }
   }
 
   onClick() {
-    console.log(this.mission);
+    //console.log(this.mission);
+    console.log(this.reminder);
   }
 }
